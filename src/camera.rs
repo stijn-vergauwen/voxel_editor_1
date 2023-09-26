0f04707ce4f4de1b310ff17894d3575a9fff9b2c
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-09-26 12:04:26 +0200

    make camera raycasts

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_rapier3d::prelude::*;
 use flying_camera::{FlyingCameraBundle, FlyingCameraPlugin};
 
 pub struct EditorCameraPlugin;
@@ -6,13 +7,65 @@ pub struct EditorCameraPlugin;
 impl Plugin for EditorCameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(FlyingCameraPlugin)
-            .add_systems(Startup, spawn_camera);
+            .add_systems(Startup, spawn_camera)
+            .add_systems(
+                Update,
+                (
+                    update_cursor_ray,
+                    update_interaction_target,
+                    draw_target_block_gizmos,
+                ),
+            );
     }
 }
 
 // TODO: make from the depths style camera, maybe replace current with it.
-// TODO: raycast utilities for interaction.
+// TODO: raycast utilities for interaction. <- doing
 // TODO: Select blocks by clicking on them
+
+#[derive(Component)]
+struct CameraInteraction {
+    distance: f32,
+    cursor_ray: Option<Ray>,
+    target: Option<TargetBlock>,
+}
+
+impl Default for CameraInteraction {
+    fn default() -> Self {
+        Self {
+            cursor_ray: None,
+            target: None,
+            distance: 20.0,
+        }
+    }
+}
+
+struct TargetBlock {
+    block_entity: Entity,
+    normal: Vec3,
+    in_position: Vec3,
+    out_position: Vec3,
+    hit_position: Vec3,
+}
+
+impl TargetBlock {
+    fn from_raycast(entity: Entity, intersection: RayIntersection) -> Self {
+        // TODO: in & out positions don't account for block size
+        let point = intersection.point;
+        let normal = intersection.normal;
+
+        let in_position = (point - normal / 2.0).round();
+        let out_position = (point + normal / 2.0).round();
+
+        Self {
+            block_entity: entity,
+            normal,
+            in_position,
+            out_position,
+            hit_position: point,
+        }
+    }
+}
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((
@@ -21,5 +74,58 @@ fn spawn_camera(mut commands: Commands) {
             ..Default::default()
         },
         FlyingCameraBundle::default(),
+        CameraInteraction::default(),
     ));
+}
+
+fn update_cursor_ray(
+    mut cameras: Query<(&mut CameraInteraction, &Camera, &GlobalTransform)>,
+    window: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(window) = window.get_single() {
+        for (mut interaction, camera, transform) in cameras.iter_mut() {
+            interaction.cursor_ray = get_cursor_as_ray(camera, transform, window);
+        }
+    }
+}
+
+fn update_interaction_target(
+    rapier_context: ResMut<RapierContext>,
+    mut cameras: Query<&mut CameraInteraction>,
+) {
+    for mut camera in cameras.iter_mut() {
+        if let Some(ray) = camera.cursor_ray {
+            let ray_origin = ray.origin;
+            let ray_dir = ray.direction;
+            let max_toi = camera.distance;
+
+            camera.target = rapier_context
+                .cast_ray_and_get_normal(ray_origin, ray_dir, max_toi, true, QueryFilter::new())
+                .map(|(entity, intersection)| TargetBlock::from_raycast(entity, intersection))
+        }
+    }
+}
+
+fn draw_target_block_gizmos(cameras: Query<&CameraInteraction>, mut gizmos: Gizmos) {
+    for camera in cameras.iter() {
+        if let Some(target) = &camera.target {
+            gizmos.sphere(target.out_position, Quat::IDENTITY, 0.5, Color::BISQUE);
+            gizmos.ray(target.hit_position, target.normal, Color::BLUE);
+        }
+    }
+}
+
+fn get_cursor_as_ray(
+    camera: &Camera,
+    global_transform: &GlobalTransform,
+    window: &Window,
+) -> Option<Ray> {
+    camera.viewport_to_world(global_transform, window.cursor_position()?)
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    // Idk how to write tests for raycasting from camera since this uses a lot of Bevy components.
 }
