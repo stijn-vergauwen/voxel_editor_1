c84b5fa56faa1ea179018cd2682d4d64ebbf7e9e
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-09-28 10:00:01 +0200

    remove blocks on event

--- src/world/interaction.rs
+++ src/world/interaction.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::camera::building::PlaceBlockRequest;
+use crate::camera::building::{PlaceBlockRequest, RemoveBlockRequest};
 
 use super::{Chunk, ChunkIndex};
 
@@ -8,7 +8,7 @@ pub struct WorldInteractionPlugin;
 
 impl Plugin for WorldInteractionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, handle_place_request);
+        app.add_systems(Update, (handle_place_request, handle_remove_request));
     }
 }
 
@@ -25,6 +25,23 @@ fn handle_place_request(
             );
 
             chunk.set_block_id(index, event.block_id);
+        }
+    }
+}
+
+fn handle_remove_request(
+    mut remove_events: EventReader<RemoveBlockRequest>,
+    mut chunks: Query<&mut Chunk>,
+) {
+    for event in remove_events.iter() {
+        for mut chunk in chunks.iter_mut() {
+            let index = ChunkIndex::new(
+                event.position.x as usize,
+                event.position.y as usize,
+                event.position.z as usize,
+            );
+
+            chunk.set_block_id(index, 0);
         }
     }
 }
