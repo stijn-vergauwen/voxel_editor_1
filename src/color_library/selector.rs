fabc84968656c22c9b8d51e94f32f68ace481658
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-10-03 09:28:56 +0200

    highlight selected color in ui

--- src/color_library/selector.rs
+++ src/color_library/selector.rs
@@ -16,20 +16,26 @@ const NEXT_COLOR_KEY: KeyCode = KeyCode::Period;
 #[derive(Component, Clone, Copy)]
 struct SelectorButton {
     color: Color,
+    pub is_highlighted: bool,
 }
 
 impl SelectorButton {
     fn new(color: Color) -> Self {
-        Self { color }
+        Self {
+            color,
+            is_highlighted: false,
+        }
     }
 
     fn to_ui(&self) -> (ButtonBundle, SelectorButton) {
         (
             ButtonBundle {
                 background_color: self.color.into(),
+                border_color: self.border_color().into(),
                 style: Style {
                     width: Val::Px(20.0),
                     height: Val::Px(20.0),
+                    border: UiRect::all(Val::Px(2.0)),
                     ..default()
                 },
                 ..default()
@@ -38,8 +44,17 @@ impl SelectorButton {
         )
     }
 
+    // TODO: move to library or delete this method. doesn't make sense here
     fn set_selected(&self, library: &mut ColorLibrary) {
         library.select_color(self.color);
+    }
+
+    fn border_color(&self) -> Color {
+        if self.is_highlighted {
+            Color::WHITE
+        } else {
+            Color::DARK_GRAY
+        }
     }
 }
 
@@ -63,10 +78,9 @@ fn spawn_selector_buttons(mut commands: Commands, color_library: Res<ColorLibrar
             ..default()
         })
         .with_children(|list| {
-            for color in color_library.all_colors() {
-                let selector_button = SelectorButton::new(color);
-
-                list.spawn(selector_button.to_ui());
+            let buttons = build_buttons_from_colors(color_library.all_colors());
+            for node in build_buttons_ui(buttons, color_library.selected_color()) {
+                list.spawn(node);
             }
         });
 }
@@ -82,11 +96,30 @@ fn button_interaction(
     }
 }
 
+fn build_buttons_from_colors(colors: Vec<Color>) -> Vec<SelectorButton> {
+    colors
+        .into_iter()
+        .map(|color| SelectorButton::new(color))
+        .collect()
+}
+
+fn build_buttons_ui(
+    buttons: Vec<SelectorButton>,
+    selected_color: Option<Color>,
+) -> Vec<(ButtonBundle, SelectorButton)> {
+    buttons
+        .into_iter()
+        .map(|mut button| {
+            button.is_highlighted = Some(button.color) == selected_color;
+            button.to_ui()
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // TODO: highlight button that has selected color
     // TODO: selecting new color highlights that button
     // TODO:
 
@@ -119,5 +152,39 @@ mod tests {
         selector.set_selected(&mut library);
 
         assert_eq!(library.selected_color(), Some(Color::GREEN));
+    }
+
+    #[test]
+    fn can_set_button_highlighted() {
+        let mut selector = SelectorButton::new(Color::GREEN);
+
+        // This test shows that highlighted should default to false, but does it provide any value?
+        assert_eq!(selector.is_highlighted, false);
+
+        selector.is_highlighted = true;
+
+        assert_eq!(selector.is_highlighted, true);
+    }
+
+    #[test]
+    fn button_highlighted_changes_border_color() {
+        let mut selector = SelectorButton::new(Color::GREEN);
+        selector.is_highlighted = true;
+
+        let node: (ButtonBundle, SelectorButton) = selector.to_ui();
+
+        assert_eq!(node.0.border_color.0, Color::WHITE);
+    }
+
+    #[test]
+    fn selected_color_gets_highlighted() {
+        let buttons = build_buttons_from_colors(vec![Color::RED, Color::GREEN]);
+        let nodes: Vec<(ButtonBundle, SelectorButton)> =
+            build_buttons_ui(buttons, Some(Color::GREEN));
+
+        let selected_button = &nodes[1].0;
+
+        assert_eq!(selected_button.background_color.0, Color::GREEN);
+        assert_eq!(selected_button.border_color.0, Color::WHITE);
     }
 }
