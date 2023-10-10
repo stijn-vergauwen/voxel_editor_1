1c70b4e6daffeccb3fb757a5437bc1ca1da46188
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-10-10 09:32:50 +0200

    errors when loading scene file, not sure how to fix

--- src/scene_loader.rs
+++ src/scene_loader.rs
@@ -8,29 +8,60 @@ pub struct SceneLoaderPlugin;
 
 impl Plugin for SceneLoaderPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, save_test_scene_on_keypress);
+        app.add_systems(Update, (handle_save_input, handle_load_input));
     }
 }
 
 const SCENE_FILE_PATH: &str = "scenes/test_scene.scn.ron";
 
-fn save_test_scene_on_keypress(world: &mut World) {
-    if !world.resource::<Input<KeyCode>>().just_pressed(KeyCode::S) {
-        return;
-    }
+const SAVE_KEY: KeyCode = KeyCode::I;
+const LOAD_KEY: KeyCode = KeyCode::O;
 
+fn handle_save_input(world: &mut World) {
+    let input = world.resource::<Input<KeyCode>>().clone();
+
+    if input.just_pressed(SAVE_KEY) {
+        save_test_scene(world);
+    }
+}
+
+fn handle_load_input(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    chunk: Query<Entity, With<Chunk>>,
+    input: Res<Input<KeyCode>>,
+) {
+    if input.just_pressed(LOAD_KEY) {
+        load_test_scene(&mut commands, &asset_server, chunk.single());
+    }
+}
+
+fn save_test_scene(world: &mut World) {
     let mut scene_world = create_empty_world(clone_type_registry(&world));
 
     for chunk in world.query::<&Chunk>().iter(world) {
         scene_world.spawn(*chunk);
     }
 
-    println!(
-        "test scene has {} entities",
-        scene_world.entities().total_count()
-    );
-
     save_world_data(String::from(SCENE_FILE_PATH), &scene_world);
+}
+
+fn load_test_scene(commands: &mut Commands, asset_server: &AssetServer, chunk_entity: Entity) {
+    // TODO: how do I transform a dynamic scene handle to chunk data? do I need to wrap the original chunk in a dynamic scene too?
+    
+    let loaded_chunk: Handle<DynamicScene> = asset_server.load(SCENE_FILE_PATH);
+    println!("{:#?}", loaded_chunk);
+
+    commands.entity(chunk_entity).despawn_recursive();
+
+    // I'm getting the message "SceneInstance is not registered in the TypeRegistry" when opening SceneInstance in bevy inspector.
+    // One thing to try out: make a separate struct ChunkData to represent the data in a way that's easier to serialize (flat array instead of nested array). Then switch between these
+    
+    // TODO: find out how to load a dynamic scene.
+    commands.spawn(DynamicSceneBundle {
+        scene: loaded_chunk,
+        ..default()
+    });
 }
 
 fn create_empty_world(type_registry: AppTypeRegistry) -> World {
@@ -41,6 +72,12 @@ fn create_empty_world(type_registry: AppTypeRegistry) -> World {
 
 fn clone_type_registry(world: &World) -> AppTypeRegistry {
     world.resource::<AppTypeRegistry>().clone()
+}
+
+fn save_world_data(file_path: String, world: &World) {
+    if let Some(serialized_data) = serialize_world_data(world) {
+        write_data_to_file(file_path, serialized_data);
+    }
 }
 
 fn serialize_world_data(world: &World) -> Option<String> {
@@ -58,10 +95,4 @@ fn write_data_to_file(file_path: String, serialized_data: String) {
                 .expect("Error while writing scene to file");
         })
         .detach();
-}
-
-fn save_world_data(file_path: String, world: &World) {
-    if let Some(serialized_data) = serialize_world_data(world) {
-        write_data_to_file(file_path, serialized_data);
-    }
 }
