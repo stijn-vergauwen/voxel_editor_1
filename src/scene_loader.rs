80ffdbc805f4844d010d5a7fe447e041ba6379b7
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-10-28 10:57:25 +0200

    setup save and load requests

--- src/scene_loader.rs
+++ src/scene_loader.rs
@@ -1,12 +1,12 @@
 use bevy::prelude::*;
 
-use crate::world::chunk::Chunk;
-
 pub struct SceneLoaderPlugin;
 
 impl Plugin for SceneLoaderPlugin {
     fn build(&self, app: &mut App) {
-        // app.add_systems(Update, ());
+        app.add_event::<OnSaveSceneRequest>()
+            .add_event::<OnLoadSceneRequest>()
+            .add_systems(Update, send_requests_on_keyboard_input);
     }
 }
 
@@ -15,8 +15,27 @@ const SCENE_FILE_PATH: &str = "scenes/test_scene.ron";
 const SAVE_KEY: KeyCode = KeyCode::I;
 const LOAD_KEY: KeyCode = KeyCode::O;
 
-// TODO: request for saving scene
-// TODO: request for loading scene
-// TODO: send requests on user input
 // TODO: save scene to test file
 // TODO: load scene from test file
+
+#[derive(Event)]
+struct OnSaveSceneRequest;
+
+#[derive(Event)]
+struct OnLoadSceneRequest;
+
+fn send_requests_on_keyboard_input(
+    input: Res<Input<KeyCode>>,
+    mut on_save_request: EventWriter<OnSaveSceneRequest>,
+    mut on_load_request: EventWriter<OnLoadSceneRequest>,
+) {
+    if input.just_pressed(SAVE_KEY) {
+        println!("Request save");
+        on_save_request.send(OnSaveSceneRequest);
+    }
+
+    if input.just_pressed(LOAD_KEY) {
+        println!("Request load");
+        on_load_request.send(OnLoadSceneRequest);
+    }
+}
