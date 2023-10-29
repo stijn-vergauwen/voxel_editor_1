0240a4daa869082d970943c21b70b257f66fadef
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-10-29 21:42:25 +0100

    make event for pressing a mouse button

--- src/mouse_interaction.rs
+++ src/mouse_interaction.rs
@@ -5,7 +5,15 @@ pub struct MouseInteractionPlugin;
 impl Plugin for MouseInteractionPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(MouseInteraction::default())
-            .add_systems(Update, update_mouse_on_ui);
+            .add_event::<OnMousePressed>()
+            .add_systems(
+                Update,
+                (
+                    update_mouse_on_ui,
+                    send_mouse_pressed_events,
+                    test_read_events,
+                ),
+            );
     }
 }
 
@@ -30,8 +38,33 @@ impl MouseInteraction {
     }
 }
 
+#[derive(Event, Clone, Copy, Debug)]
+pub struct OnMousePressed {
+    pub button: MouseButton,
+    pub on_ui: bool,
+}
+
 fn update_mouse_on_ui(mut mouse_interaction: ResMut<MouseInteraction>, nodes: Query<&Interaction>) {
     mouse_interaction.mouse_on_ui = nodes.iter().any(|interaction| {
         *interaction == Interaction::Hovered || *interaction == Interaction::Pressed
     });
+}
+
+fn send_mouse_pressed_events(
+    mouse_interaction: Res<MouseInteraction>,
+    input: Res<Input<MouseButton>>,
+    mut on_mouse_pressed: EventWriter<OnMousePressed>,
+) {
+    for press in input.get_just_pressed() {
+        on_mouse_pressed.send(OnMousePressed {
+            button: *press,
+            on_ui: mouse_interaction.mouse_on_ui,
+        });
+    }
+}
+
+fn test_read_events(mut on_mouse_pressed: EventReader<OnMousePressed>) {
+    for event in on_mouse_pressed.iter() {
+        println!("{:?}", event);
+    }
 }
