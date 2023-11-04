c92590602ad6da398efed77c45ceba08fd71f456
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-11-04 16:41:59 +0100

    clear selected blocks when leaving select mode

--- src/player/editor_modes/select.rs
+++ src/player/editor_modes/select.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::{newtypes::coordinate::Coordinate, player::mouse_interaction::OnMousePressed, world::WorldSettings};
+use crate::{
+    newtypes::coordinate::Coordinate, player::mouse_interaction::OnMousePressed,
+    world::WorldSettings,
+};
 
 use super::EditorMode;
 
@@ -9,13 +12,23 @@ pub struct SelectModePlugin;
 impl Plugin for SelectModePlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<CurrentSelection>()
-            .add_systems(Update, (handle_input, draw_current_selection).run_if(in_state(EditorMode::Select)));
+            .add_systems(
+                Update,
+                (handle_input, draw_current_selection).run_if(in_state(EditorMode::Select)),
+            )
+            .add_systems(OnExit(EditorMode::Select), clear_current_selection);
     }
 }
 
 #[derive(Resource, Debug, Default)]
 pub struct CurrentSelection {
     coordinates: Vec<Coordinate>,
+}
+
+impl CurrentSelection {
+    fn clear_selection(&mut self) {
+        self.coordinates.clear();
+    }
 }
 
 fn handle_input(
@@ -36,6 +49,10 @@ fn toggle_coordinate_in_selection(coord: Coordinate, selection: &mut CurrentSele
     } else {
         selection.coordinates.push(coord);
     }
+}
+
+fn clear_current_selection(mut current_selection: ResMut<CurrentSelection>) {
+    current_selection.clear_selection();
 }
 
 fn draw_current_selection(
